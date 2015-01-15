13466ed6d7ab7f5930388f26d36517f17ff64c18
binitkumar <binit.kumar.hbs@gmail.com>
2015-01-15 00:27:19 -0500

    updated fix

--- Gemfile
+++ Gemfile
@@ -4,8 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 # Use sqlite3 as the database for Active Record
-gem 'pg'
-gem 'mysql2'
+gem 'mysql2', "0.3.17"
 # Use SCSS for stylesheets
 #gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
@@ -19,7 +18,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+#gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,32 @@ class Task < ActiveRecord::Base
   belongs_to :organization
   has_and_belongs_to_many :persons
   has_many :task_details, ->{ order("created_at asc") }
-  validates_presence_of :title
 
+
+  before_save :associate_users
+
+  def associate_users
+    persons = Array.new
+    self.user.organizations.each do |org|
+      org.persons.each do |per|
+        persons.push per
+      end
+    end
+    persons.each do |pers|
+      if self.title.match(/#{pers.name}/)
+        begin
+          self.task.persons.push pers
+        rescue => exp
+
+        end
+      end
+    end
+    self.title.gsub!(/\@\[\[\d\:\:/," ") 
+    self.title.gsub!(/\]\]/, " ") 
+    begin 
+      self.task.save 
+    rescue => exp
+
+    end
+  end
 end
